bb166b87f8887c4022e47c925530ed6b9c354e67
BongoTech <c_mckiel@yahoo.com>
2020-10-20 14:43:41 -0500

    user attaches and reads shared memory.

--- user.c
+++ user.c
@@ -1,7 +1,62 @@
+#include <errno.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
+
+//*****************************************************
+////BEGIN: Setting up shared memory.
+
+    int i = 0;
+    
+    key_t key;
+    int shmid;
+    int *shmp;
+
+    //Generate key deterministically so that children
+    //can do the same and attach to shared memory.
+    if ( (key = ftok("./", 912)) == -1 ) {
+        fprintf(stderr, "%s: Error: ftok() failed to generated key.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+    //Calculate the size of shared memory.
+    //One slot for seconds, one for nanoseconds,
+    //and one for child pid. Total three.
+    int shm_size = 3;
+
+    //Create and get the id of the shared memory segment.
+    if ( (shmid = shmget(key, shm_size, 0666)) < 0 ) {
+        fprintf(stderr, "%s: Error: Failed to allocate shared memory.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+    //Attach to shared memory.
+    if ( (shmp = (int*)shmat(shmid, NULL, 0)) < 0 ) {
+        fprintf(stderr, "%s: Error: Failed to attach to shared memory.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+    for (i = 0; i < shm_size; i++) {
+       printf("User: shmp at %d: %d\n", i, *(shmp + i)); 
+    }
+
+
+
+//END: Setting up shared memory.
+
     printf("Hello from user.\n");
+
+    shmdt(shmp);
+
     return 0;
 }
