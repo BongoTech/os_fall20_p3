0daa4b9c91ef7dfd8a77e6c15610e04346349c88
BongoTech <c_mckiel@yahoo.com>
2020-10-20 10:10:33 -0500

    oss creates, uses, and deallocates shared memory.

--- oss.c
+++ oss.c
@@ -37,9 +37,12 @@ int help(char*);
 //*****************************************************
 int main(int argc, char *argv[])
 {
-//*****************************************************
-//BEGIN: Command line processing.
-    
+    //*****************************************************
+    //BEGIN: Command line processing.
+
+    //Used throughout the program for iterating.
+    int i = 0; 
+
     int mx_chdrn = 5;
     int ptime = 20;
     char logfile[FILENAMESIZE] = "log.out";
@@ -72,6 +75,54 @@ int main(int argc, char *argv[])
     printf("Logfile name: %s\n", logfile);
     printf("Time to run: %d\n", ptime);
 
+    //END: Command line processing.
+    //*****************************************************
+    //BEGIN: Setting up shared memory.
+
+    key_t key;
+    int shmid;
+    int *shmp;
+
+    //Generate key deterministically so that children
+    //can do the same and attach to shared memory.
+    if ( (key = ftok("./", 912)) == -1 ) {
+        fprintf(stderr, "%s: Error: ftok() failed to generated key.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+    //Calculate the size of shared memory.
+    //One slot for seconds, one for nanoseconds,
+    //and one for child pid. Total three.
+    int shm_size = 3;
+
+    //Create and get the id of the shared memory segment.
+    if ( (shmid = shmget(key, shm_size, IPC_CREAT|0666)) < 0 ) {
+        fprintf(stderr, "%s: Error: Failed to allocate shared memory.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+    //Attach to shared memory.
+    if ( (shmp = (int*)shmat(shmid, NULL, 0)) < 0 ) {
+        fprintf(stderr, "%s: Error: Failed to attach to shared memory.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+    for (i = 0; i < shm_size; i++) {
+        *(shmp + i) = 0;
+    }
+    for (i = 0; i < shm_size; i++) {
+       printf("shmp at %d: %d\n", i, *(shmp + i)); 
+    }
+
+
+
+//END: Setting up shared memory.
+//*****************************************************
+//BEGIN: Finishing up.
+
+    //Clean up shared memory.
+    shmdt(shmp);
+    shmctl(shmid, IPC_RMID, 0);
 
     return 0;
 }
